4ab0782b7ecf134c9a7a04b782a408bafe923e98
jshprintz <jshprintz@gmail.com>
2022-07-15 13:55:25 -0700

    Updates to button project

--- dom-events/js/app.js
+++ dom-events/js/app.js
@@ -2,11 +2,24 @@ console.log("Javascript works");
 
 //Selecting button to add eventlistener to.
 const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
 
 //add the event listener.
 
 buttonEl.addEventListener('click', function(e){
-    console.log(e);
-    console.log('button is working');
-})
+    // Pull value
+    const inputEl = document.querySelector('input');
+
+    // Add item    
+    const listItem = document.createElement('li');
+    listItem.innerText = inputEl.value;
+    document.querySelector('ul').appendChild(listItem);
+
+    // Clear text window
+    inputEl.value = '';
+});
+
+
+
+
+// Add a list item that contains the text of the input, and 
+// put it inside of the ul when we click on the button.
