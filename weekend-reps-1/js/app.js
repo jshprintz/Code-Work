6bc48e9e37d6d63b16be5331ec5a36ba718ee4b5
jshprintz <jshprintz@gmail.com>
2022-07-15 20:02:49 -0700

    Starting Weekend Work

---
+++ weekend-reps-1/js/app.js
@@ -0,0 +1,15 @@
+console.log("Javascript works");
+
+// B
+//1
+let firstVariable = 'Hello World';
+firstVariable = 420;
+let secondVariable = firstVariable;
+secondVariable = 'Im tired';
+console.log(firstVariable);
+
+//2
+const yourName = 'Jason Shprintz';
+console.log('Hello, my name is ' + yourName);
+
+
