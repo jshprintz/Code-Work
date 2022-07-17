8707d4978cecdd80de3e2aa2b71429802eee49b3
jshprintz <jshprintz@gmail.com>
2022-07-16 17:12:07 -0700

    click works

--- calculator-lab/js/main.js
+++ calculator-lab/js/main.js
@@ -1,2 +1,56 @@
 console.log("js works!");
 
+// const buttonOne = document.getElementById('one');
+// const buttonTwo = document.getElementById('two');
+// const buttonThree = document.getElementById('three');
+// const buttonFour = document.getElementById('four');
+// const buttonFive = document.getElementById('five');
+// const buttonSix = document.getElementById('six');
+// const buttonSeven = document.getElementById('seven');
+// const buttonEight = document.getElementById('eight');
+// const buttonNine = document.getElementById('nine');
+// const buttonZero = document.getElementById('zero');
+// const buttonAdd = document.getElementById('plus');
+// const buttonSubtract = document.getElementById('minus');
+// const buttonMultiply = document.getElementById('multiply');
+// const buttonDivide = document.getElementById('divide');
+// const buttonClear = document.getElementById('clear');
+
+const buttonSelect = document.querySelector('body');
+
+buttonSelect.addEventListener('click', function(){
+    console.log("Worked")
+}) 
+;
+
+
+
+// Add function
+function addition(firstNum){
+    let sum = firstNum
+    return sum;
+};
+
+// Subtract function
+function subtraction(firstNum){
+    let sum = firstNum
+    return sum;
+};
+
+// Multiplication function
+function multiply(firstNum){
+    let sum = firstNum
+    return sum;
+};
+
+// Divide function
+function divide(firstNum){
+    let sum = firstNum
+    return sum;
+};
+
+// Equals function
+function equals(firstNum){
+    let sum = firstNum
+    return sum;
+};
