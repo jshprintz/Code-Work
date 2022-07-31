5abe6d290a6df8176f06e81f5d392290ce0e026f
jshprintz <jshprintz@gmail.com>
2022-07-31 16:47:29 -0700

    Lesson 27

--- node-practice/app.js
+++ node-practice/app.js
@@ -1,1 +1,31 @@
-console.log('Hello world');
+// Required it first
+let express = require('express');
+
+// Declare it into a varaible
+let app = express();
+
+app.set('view engine', 'ejs');
+
+// Homepage Get request
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Contact get request
+app.get('/contact', function(req, res){
+    res.sendFile(__dirname + '/contact.html');
+});
+
+// Respond to a particular request
+app.get('/profile/:name', function(req, res){
+    let data = {
+        age: 29,
+        job: 'ninja',
+        hobbies: ['eating', 'fighting', 'fishing'],
+    };
+    res.render('profile', {person: req.params.name, data: data});
+});
+
+// host
+app.listen(3000);
+
